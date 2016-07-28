2c26f31f79cb880d1f937d2b7725e62420576310
Tobias Viehweger <tobias.viehweger@gmx.de>
2016-07-28 16:56:47 +0200

    remove test import

--- src/android/QRScanner.java
+++ src/android/QRScanner.java
@@ -28,7 +28,6 @@ import android.hardware.Camera;
 import android.provider.Settings;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.HashMap;
