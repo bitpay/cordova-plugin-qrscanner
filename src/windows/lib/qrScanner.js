c55f672a5871527c6f2eb40ae2b12542e42c752b
Nick Cardin <nick@cardin.email>
2017-03-22 13:06:36 -0400

    feat(windows): target windows 10 universal

--- src/windows/lib/qrScanner.js
+++ src/windows/lib/qrScanner.js
@@ -22,243 +22,265 @@ const cameraTypes = {
   FRONT: 1
 };
 
-function create() {
+const defaultStatusFlags = {
+  prepared: false,
+  authorized: false,
+  denied: false,
+  restricted: false,
+  scanning: false,
+  previewing: false,
+  showing: false,
+  lightEnabled: false,
+  canOpenSettings: false,
+  canEnableLight: false,
+  canChangeCamera: false,
+  currentCamera: cameraTypes.BACK
+};
 
-  const defaultStatusFlags = {
-    prepared: false,
-    authorized: false,
-    denied: false,
-    restricted: false,
-    scanning: false,
-    previewing: false,
-    showing: false,
-    lightEnabled: false,
-    canOpenSettings: false,
-    canEnableLight: false,
-    canChangeCamera: false,
-    currentCamera: cameraTypes.BACK
-  };
+let statusFlags;
+let currentVideoCapture;
 
-  function getDefaultStatusFlags() {
-    let statusFlags = {};
-    for (let property in defaultStatusFlags) {
-      statusFlags[property] = defaultStatusFlags[property];
-    }
-    return statusFlags;
+function resetStatusFlags() {
+
+  statusFlags = {};
+  for (let property in defaultStatusFlags) {
+    statusFlags[property] = defaultStatusFlags[property];
   }
+  return statusFlags;
+}
 
-  let statusFlags = getDefaultStatusFlags();
-  let availableCameras;
-  let currentVideoCapture;
-  let currentPreview;
+function reset() {
+  document.body.removeEventListener('click', onPreviewClick);
+  preview.destroy();
+  currentVideoCapture = null;
+  availableCameras = null;
+  resetStatusFlags();
+}
 
-  function generateStatusResponse() {
-    let response = {};
-    for (let property in statusFlags) {
-      response[property] = statusFlags[property] ? '1' : '0';
-    }
-    return Promise.wrap(response);
+function generateStatusResponse() {
+  let response = {};
+  for (let property in statusFlags) {
+    response[property] = statusFlags[property] ? '1' : '0';
   }
+  return Promise.wrap(response);
+}
 
-  function init() {
-    if (!statusFlags.prepared) {
-      availableCameras = null;
-      statusFlags.currentCamera = null;
-      currentVideoCapture = null;
-      currentPreview = preview.create();
-      document.body.addEventListener('click', onPreviewClick);
-      return videoCapture.getCameras().then(function (cameras) {
-        if (cameras.back && cameras.front) {
-          statusFlags.canChangeCamera = true;
-        }
-        availableCameras = cameras;
-        return initCamera(cameraTypes.BACK).then(function () {
-          statusFlags.prepared = true;
-          statusFlags.authorized = true;
-        }, function (error) {
-          if (error.message.indexOf('Access is denied') > -1) {
-            statusFlags.denied = true;
-            return Promise.wrapError(errorTypes.CAMERA_ACESS_DENIED);
-          }
-          return Promise.wrapError(errorTypes.UNEXPECTED_ERROR);
-        });
-      });
-    }
-    return Promise.wrap();
-  }
-
-  function initCamera(cameraType) {
-    if (statusFlags.currentCamera !== cameraType) {
-      if (cameraType === cameraTypes.FRONT && !availableCameras.front) {
-        cameraType = cameraTypes.BACK;
+function init() {
+  if (!statusFlags.prepared) {
+    document.body.addEventListener('click', onPreviewClick);
+    return videoCapture.getCameras().then(function (cameras) {
+      if (cameras.back && cameras.front) {
+        statusFlags.canChangeCamera = true;
       }
-
-      return videoCapture.get(cameraType ? availableCameras.front.id : availableCameras.back.id).then(function (videoCapture) {
-        currentVideoCapture = videoCapture;
-
-        return Promise.join({
-          videoUrl: currentVideoCapture.getUrl(),
-          canEnableLight: currentVideoCapture.canEnableLight(),
-          capture: currentVideoCapture.getCapture()
-        }).then(function (result) {
-          if (statusFlags.showing) {
-            currentPreview.pause();
-          }
-          currentPreview.setVideoUrl(result.videoUrl);
-          currentPreview.setMirroring(cameraType === cameraTypes.FRONT);
-          if (statusFlags.showing) {
-            currentPreview.resume();
-          }
-          statusFlags.canEnableLight = result.canEnableLight;
-          statusFlags.currentCamera = cameraType;
-          barcodeReader.setCapture(result.capture);
-        });
-      });
-
-    }
-    return Promise.wrap();
-  }
-
-  function onPreviewClick(e) {
-    if (statusFlags.showing && currentVideoCapture) {
-      currentVideoCapture.focus();
-    }
-  }
-
-  let qrScanner = {};
-
-  qrScanner.getStatus = function () {
-    return init().then(generateStatusResponse, generateStatusResponse);
-  }
-
-  qrScanner.prepare = function () {
-    return init().then(generateStatusResponse);
-  }
-
-  qrScanner.useCamera = function (inputStr) {
-    return init().then(function () {
-      let cameraType = parseInt(inputStr)
-      return initCamera(cameraType).then(function () {
-        return generateStatusResponse();
+      if (!cameras.back && !cameras.front) {
+        return Promise.wrapError(errorTypes.CAMERA_UNAVAILABLE);
+      }
+      availableCameras = cameras;
+      return initCamera().then(function () {
+        statusFlags.prepared = true;
+        statusFlags.authorized = true;
       });
     });
   }
+  return Promise.wrap();
+}
 
-  qrScanner.show = function () {
-    return init().then(function (preview) {
-      currentPreview.show();
-      statusFlags.showing = true;
+function initCamera(cameraType) {
+  if (cameraType === cameraTypes.BACK && !availableCameras.back) {
+    return Promise.wrapError(errorTypes.BACK_CAMERA_UNAVAILABLE);
+  }
+  if (cameraType === cameraTypes.FRONT && !availableCameras.front) {
+    return Promise.wrapError(errorTypes.FRONT_CAMERA_UNAVAILABLE);
+  }
+  if (!cameraType) {
+    if (availableCameras.front) {
+      cameraType = cameraTypes.FRONT;
+    }
+    if (availableCameras.back) {
+      cameraType = cameraTypes.BACK;
+    }
+  }
+  preview.setVideoUrl('');
+  return videoCapture.get(cameraType ? availableCameras.front.id : availableCameras.back.id).then(function (videoCapture) {
+    currentVideoCapture = videoCapture;
+
+    let videoUrl = currentVideoCapture.url;
+
+    if (statusFlags.showing) {
+      preview.pause();
+    }
+    preview.setVideoUrl(videoUrl);
+    preview.setMirroring(cameraType === cameraTypes.FRONT);
+    if (statusFlags.showing) {
+      preview.resume();
+    }
+    barcodeReader.setCapture(currentVideoCapture.capture);
+    statusFlags.canEnableLight = currentVideoCapture.canEnableLight;
+    statusFlags.currentCamera = cameraType;
+
+  }, function (error) {
+    if (error.message.indexOf('Access is denied') > -1) {
+      statusFlags.denied = true;
+      return Promise.wrapError(errorTypes.CAMERA_ACESS_DENIED);
+    }
+    return Promise.wrapError(errorTypes.UNEXPECTED_ERROR);
+  });
+}
+
+function onPreviewClick(e) {
+  if (statusFlags.showing && currentVideoCapture) {
+    currentVideoCapture.focus();
+  }
+}
+
+let qrScanner = {};
+
+qrScanner.getStatus = function () {
+  return init().then(generateStatusResponse, generateStatusResponse);
+}
+
+qrScanner.prepare = function () {
+  return init().then(generateStatusResponse);
+}
+
+qrScanner.useCamera = function (inputStr) {
+  return init().then(function () {
+    let cameraType = parseInt(inputStr);
+    return initCamera(cameraType).then(function () {
       return generateStatusResponse();
     });
-  }
+  });
+}
 
-  qrScanner.hide = function () {
-    return init().then(function (preview) {
-      currentPreview.hide();
-      statusFlags.showing = false;
-      return generateStatusResponse();
-    });
-  }
+qrScanner.show = function () {
+  return init().then(function () {
+    preview.show();
+    statusFlags.showing = true;
+    statusFlags.previewing = preview.isPlaying();
+    return generateStatusResponse();
+  });
+}
 
-  let resolveLastScanPromise, rejectLastScanPromise;
+qrScanner.hide = function () {
+  return init().then(function () {
+    preview.hide();
+    statusFlags.showing = false;
+    statusFlags.previewing = false;
+    return generateStatusResponse();
+  });
+}
 
-  qrScanner.scan = function () {
+let resolveLastScanPromise, rejectLastScanPromise;
 
-    if (statusFlags.scanning) {
-      rejectLastScanPromise(errorTypes.SCAN_CANCELED);
+qrScanner.scan = function () {
 
-      let lastScanPromise = new Promise(function (resolve, reject) {
-        resolveLastScanPromise = resolve;
-        rejectLastScanPromise = reject;
-      });
-
-      return lastScanPromise;
-
-    }
-    statusFlags.scanning = true;
+  if (statusFlags.scanning) {
+    rejectLastScanPromise(errorTypes.SCAN_CANCELED);
 
     let lastScanPromise = new Promise(function (resolve, reject) {
       resolveLastScanPromise = resolve;
       rejectLastScanPromise = reject;
     });
 
-    init().then(function () {
-      barcodeReader.readCode().then(function (result) {
-        if (!result) {
-          return rejectLastScanPromise(errorTypes.SCAN_CANCELED);
-        }
-        resolveLastScanPromise(result.text);
-        statusFlags.scanning = false;
-      });
-    });
-
     return lastScanPromise;
 
   }
 
-  qrScanner.cancelScan = function () {
-    statusFlags.scanning = false;
-    barcodeReader.stop();
-    return generateStatusResponse();
-  }
+  let lastScanPromise = new Promise(function (resolve, reject) {
+    resolveLastScanPromise = resolve;
+    rejectLastScanPromise = reject;
+  });
 
-  qrScanner.pausePreview = function () {
-    currentPreview.pause();
-    return generateStatusResponse();
-  }
-
-  qrScanner.resumePreview = function () {
-    currentPreview.resume();
-    return generateStatusResponse();
-  }
-
-  //on Lumia devices, light functionality may be disabled while plugged in
-  qrScanner.enableLight = function () {
-    return init().then(function () {
-      if (statusFlags.lightEnabled) {
-        return generateStatusResponse();
+  init().then(function () {
+    barcodeReader.readCode().then(function (result) {
+      if (!result) {
+        return rejectLastScanPromise(errorTypes.SCAN_CANCELED);
       }
-
-      return currentVideoCapture.enableLight().then(function (lightEnabled) {
-        statusFlags.lightEnabled = lightEnabled;
-
-        if (!statusFlags.lightEnabled) {
-          return Promise.wrapError(errorTypes.LIGHT_UNAVAILABLE);
-        }
-
-        return generateStatusResponse();
-      });
+      resolveLastScanPromise(result.text);
+      statusFlags.scanning = false;
     });
-  }
+  }, function (error) {
+    statusFlags.scanning = false;
+    return rejectLastScanPromise(error);
+  });
 
-  qrScanner.disableLight = function () {
+  statusFlags.scanning = true;
 
-    if (statusFlags.lightEnabled) {
-      return currentVideoCapture.disableLight().then(function () {
-        statusFlags.lightEnabled = false;
-        return generateStatusResponse();
-      });
-    }
-
-    return generateStatusResponse();
-
-  }
-
-  qrScanner.openSettings = function () {
-    return Promise.wrapError(errorTypes.OPEN_SETTINGS_UNAVAILABLE);
-  }
-
-  qrScanner.destroy = function () {
-    statusFlags = getDefaultStatusFlags();
-    if (currentPreview) {
-      document.body.removeEventListener('click', onPreviewClick);
-      currentPreview.destroy();
-    }
-    return generateStatusResponse();
-  }
-
-  return qrScanner;
+  return lastScanPromise;
 
 }
 
-module.exports = create();
+qrScanner.cancelScan = function () {
+  if (!statusFlags.scanning) return generateStatusResponse();
+  statusFlags.scanning = false;
+  barcodeReader.stop();
+  return generateStatusResponse();
+}
+
+qrScanner.pausePreview = function () {
+  preview.pause();
+  statusFlags.previewing = false;
+  return generateStatusResponse();
+}
+
+qrScanner.resumePreview = function () {
+  preview.resume();
+  statusFlags.previewing = statusFlags.showing;
+  return generateStatusResponse();
+}
+
+//on Lumia devices, light functionality may be disabled while plugged in
+qrScanner.enableLight = function () {
+  return init().then(function () {
+    if (statusFlags.lightEnabled) {
+      return generateStatusResponse();
+    }
+
+    statusFlags.lightEnabled = currentVideoCapture.enableLight();
+
+    if (!statusFlags.lightEnabled) {
+      return Promise.wrapError(errorTypes.LIGHT_UNAVAILABLE);
+    }
+
+    return generateStatusResponse();
+  });
+}
+
+qrScanner.disableLight = function () {
+
+  if (statusFlags.lightEnabled) {
+    currentVideoCapture.disableLight();
+  }
+
+  return generateStatusResponse();
+
+}
+
+qrScanner.openSettings = function () {
+  return Promise.wrapError(errorTypes.OPEN_SETTINGS_UNAVAILABLE);
+}
+
+qrScanner.destroy = function () {
+  reset();
+  return generateStatusResponse();
+}
+
+reset();
+
+function wrapPromise(fn) {
+  return function (successCallback, errorCallback, strInput) {
+    fn.call(window, strInput).then(successCallback, function (errorCode) {
+      errorCallback(errorCode.toString() || '0');
+    });
+  }
+}
+
+for (let property in qrScanner) {
+  if (typeof qrScanner[property] == "function") {
+    exports[property] = wrapPromise(qrScanner[property])
+  }
+}
+
+module.exports = exports;
+
+cordova.commandProxy.add("QRScanner", exports);
