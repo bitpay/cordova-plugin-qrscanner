ffaa26c77882076c6b6b49bc77639edc1754cacc
あ <>
2018-01-21 22:48:40 +0900

    Why is webpack.library == webpack.prepack ??????? Why didn't cordova-plugin-qrscanner-lib.min.js exist??????

--- webpack.library.config.js
+++ webpack.library.config.js
@@ -3,11 +3,13 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    worker: path.join(__dirname, './src/browser/src/worker.js'),
+    worker: path.join(__dirname, './src/browser/src/library.js'),
   },
   output: {
-    path: path.join(__dirname, './src/browser'),
-    filename: 'worker.min.js'
+    path: path.join(__dirname, './dist'),
+    filename: 'cordova-plugin-qrscanner-lib.min.js',
+    library: 'QRScanner',
+    libraryTarget: 'var'
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
